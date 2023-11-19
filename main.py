dffd48faf10a416c8083b48fb784a9cbe135b24a
cgint <christian.gintenreiter@gmail.com>
2023-11-19 13:00:25 +0100

    Make things work

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import autogen
+import os
 
 config_list = [
     {
-        'model': 'gpt-4',
-        'api_key': 'API_KEY'
+        # 'model': 'gpt-3.5-turbo-16k',
+        'model': 'gpt-4-1106-preview',
+        'api_key': os.environ.get("OPENAI_API_KEY")
     }
 ]
 
-llm_config={
+llm_config = {
     "request_timeout": 600,
     "seed": 42,
     "config_list": config_list,
@@ -17,7 +19,8 @@ llm_config={
 assistant = autogen.AssistantAgent(
     name="CTO",
     llm_config=llm_config,
-    system_message="Chief technical officer of a tech company"
+    system_message="""Chief technical officer of a tech company with long experience in software development.
+Stick to the task. Do not be chatty."""
 )
 
 user_proxy = autogen.UserProxyAgent(
@@ -25,14 +28,15 @@ user_proxy = autogen.UserProxyAgent(
     human_input_mode="NEVER",
     max_consecutive_auto_reply=10,
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
-    code_execution_config={"work_dir": "web"},
+    code_execution_config={"work_dir": "web", "use_docker": False},
     llm_config=llm_config,
     system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
-Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
+Otherwise, reply CONTINUE, or the reason why the task is not solved yet. 
+Stick to the task. Do not be chatty."""
 )
 
 task = """
-Write python code to output numbers 1 to 100, and then store the code in a file
+Write python code to output numbers 1 to 100, and then store the python code in a file named "test1.py"
 """
 
 user_proxy.initiate_chat(
@@ -41,7 +45,7 @@ user_proxy.initiate_chat(
 )
 
 task2 = """
-Change the code in the file you just created to instead output numbers 1 to 200
+Change the code in the file you just created to instead output numbers 1 to 200. store the python code in a file named "test2.py"
 """
 
 user_proxy.initiate_chat(
