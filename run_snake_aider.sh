b43a6fa7fdc5b8c17b78b57799298ccc2addbf3a
Christian Gintenreiter <christian.gintenreiter@gmail.com>
2023-12-01 16:28:22 +0100

    ignore more

--- run_snake_aider.sh
+++
@@ -1,1 +0,0 @@
-docker run -it --rm -v $(pwd)/snake_game.py:/snake_game.py -v $(pwd)/requirements.txt:/requirements.txt python:latest /bin/bash -c "pip install -r /requirements.txt && python /snake_game.py"
