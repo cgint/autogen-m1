754b8b963c1f164d12f5813c085e79c184f8b105
Christian Gintenreiter <christian.gintenreiter@gmail.com>
2023-12-01 16:16:47 +0100

    aider: Refactor snake game to handle game over conditions and store game state to disk.
    # Aider chat conversation:
    
    USER: create a snake game for command line that i can play with arrow keys in python and store it to disk
    
    ASSISTANT: To create a snake game for the command line that can be played with arrow keys and stores the game state to disk, we will need to write the game logic and handle user input for the arrow keys. We will also need to write the game state to a file when the game ends.
    
    Since you have provided two *read-write* files, `snake_aider.py` and `snake_game_gpt4.py`, I will assume that `snake_game_gpt4.py` is the main game file and `snake_aider.py` can be used for any helper functions if needed.
    
    Let's start by implementing the snake game in `snake_game_gpt4.py`:
    
    snake_game_gpt4.py
    ```python
    <<<<<<< SEARCH
    =======
    import random
    import curses
    
    def save_game_state(score):
        with open('game_state.txt', 'w') as f:
            f.write('Game Over\n')
            f.write(f'Score: {score}\n')
    
    def main(stdscr):
        # Initial setup
        curses.curs_set(0)
        sh, sw = stdscr.getmaxyx()
        w = curses.newwin(sh, sw, 0, 0)
        w.keypad(1)
        w.timeout(100)
    
        # Initial snake and food positions
        snk_x = sw//4
        snk_y = sh//2
        snake = [
            [snk_y, snk_x],
            [snk_y, snk_x-1],
            [snk_y, snk_x-2]
        ]
        food = [sh//2, sw//2]
        w.addch(food[0], food[1], curses.ACS_PI)
    
        # Initial direction
        key = curses.KEY_RIGHT
    
        while True:
            next_key = w.getch()
            key = key if next_key == -1 else next_key
    
            # Check for game over conditions
            if (snake[0][0] in [0, sh] or
                snake[0][1] in [0, sw] or
                snake[0] in snake[1:]):
                save_game_state(len(snake) - 3)
                curses.endwin()
                quit()
    
            # Calculate new head position
            new_head = [snake[0][0], snake[0][1]]
    
            if key == curses.KEY_DOWN:
                new_head[0] += 1
            if key == curses.KEY_UP:
                new_head[0] -= 1
            if key == curses.KEY_LEFT:
                new_head[1] -= 1
            if key == curses.KEY_RIGHT:
                new_head[1] += 1
    
            # Insert new head
            snake.insert(0, new_head)
    
            # Check if snake got the food
            if snake[0] == food:
                food = None
                while food is None:
                    nf = [
                        random.randint(1, sh-1),
                        random.randint(1, sw-1)
                    ]
                    food = nf if nf not in snake else None
                w.addch(food[0], food[1], curses.ACS_PI)
            else:
                # Move the snake
                tail = snake.pop()
                w.addch(tail[0], tail[1], ' ')
    
            # Add the new head
            w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
    
    # Run the game
    curses.wrapper(main)
    >>>>>>> REPLACE
    ```
    
    This code sets up the snake game with the following features:
    - Use of arrow keys for movement.
    - A game window that fits the terminal size.
    - A snake that grows when eating food.
    - A game over condition when the snake hits the wall or itself.
    - Storing the game state to `game_state.txt` when the game ends.
    
    To play the game, run `snake_game_gpt4.py` in your terminal. Make sure you have the `curses` library available in your Python environment, as it is used for handling the terminal window and keyboard input. If you're using Windows, you might need to install the `windows-curses` package to enable support for the `curses` module.

--- snake_game_gpt4.py
+++ snake_game_gpt4.py
@@ -62,3 +62,80 @@ def main(stdscr):
         w.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
 
 curses.wrapper(main)
+import random
+import curses
+
+def save_game_state(score):
+    with open('game_state.txt', 'w') as f:
+        f.write('Game Over\n')
+        f.write(f'Score: {score}\n')
+
+def main(stdscr):
+    # Initial setup
+    curses.curs_set(0)
+    sh, sw = stdscr.getmaxyx()
+    w = curses.newwin(sh, sw, 0, 0)
+    w.keypad(1)
+    w.timeout(100)
+
+    # Initial snake and food positions
+    snk_x = sw//4
+    snk_y = sh//2
+    snake = [
+        [snk_y, snk_x],
+        [snk_y, snk_x-1],
+        [snk_y, snk_x-2]
+    ]
+    food = [sh//2, sw//2]
+    w.addch(food[0], food[1], curses.ACS_PI)
+
+    # Initial direction
+    key = curses.KEY_RIGHT
+
+    while True:
+        next_key = w.getch()
+        key = key if next_key == -1 else next_key
+
+        # Check for game over conditions
+        if (snake[0][0] in [0, sh] or
+            snake[0][1] in [0, sw] or
+            snake[0] in snake[1:]):
+            save_game_state(len(snake) - 3)
+            curses.endwin()
+            quit()
+
+        # Calculate new head position
+        new_head = [snake[0][0], snake[0][1]]
+
+        if key == curses.KEY_DOWN:
+            new_head[0] += 1
+        if key == curses.KEY_UP:
+            new_head[0] -= 1
+        if key == curses.KEY_LEFT:
+            new_head[1] -= 1
+        if key == curses.KEY_RIGHT:
+            new_head[1] += 1
+
+        # Insert new head
+        snake.insert(0, new_head)
+
+        # Check if snake got the food
+        if snake[0] == food:
+            food = None
+            while food is None:
+                nf = [
+                    random.randint(1, sh-1),
+                    random.randint(1, sw-1)
+                ]
+                food = nf if nf not in snake else None
+            w.addch(food[0], food[1], curses.ACS_PI)
+        else:
+            # Move the snake
+            tail = snake.pop()
+            w.addch(tail[0], tail[1], ' ')
+
+        # Add the new head
+        w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
+
+# Run the game
+curses.wrapper(main)
